e090dd8491711ef30b0615ea7c89f9a8b8deb8bf
Gavin Joyce <gavinjoyce@gmail.com>
2015-09-12 13:46:00 +0100

    added href with query param value from a CP

--- tests/dummy/app/controllers/about.js
+++ tests/dummy/app/controllers/about.js
@@ -4,6 +4,9 @@ export default Em.Controller.extend({
   queryParams: ['section'],
   section: 'one',
   dynamic: 'hello',
+  dynamicLength: Em.computed('dynamic', function() {
+    return this.get('dynamic.length');
+  }),
   count: 0,
   actions: {
     increment: function() {
