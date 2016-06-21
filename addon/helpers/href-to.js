fbed8efa0174ced3c3517941ad3dcbb5a9e0882c
cibernox <miguel.camba@gmail.com>
2016-06-21 22:24:25 +0100

    It works

--- addon/helpers/href-to.js
+++ addon/helpers/href-to.js
@@ -2,9 +2,15 @@ import Em from 'ember';
 import getOwner from 'ember-getowner-polyfill';
 
 export default Em.Helper.extend({
+  // _routing: Em.inject.service('-routing'),
+
+  // onQpsChange: Em.observer('_routing.currentState', function() {
+  //   this.recompute();
+  // }),
+
   compute(params) {
     let router = getOwner(this).lookup('router:main');
-    if(router === undefined || router.router === undefined) {
+    if (router === undefined || router.router === undefined) {
       return;
     }
 
@@ -12,14 +18,15 @@ export default Em.Helper.extend({
 
     let queryParams = {};
     if (lastParam && lastParam.isQueryParams) {
-      queryParams = params.pop();
+      queryParams = params.pop().values;
     }
-
+    // let currentQueryParams = this.get('_routing.currentState.routerJsState.queryParams');
+    // queryParams = Em.merge({}, currentQueryParams, queryParams);
     let targetRouteName = params.shift();
 
     let args = [targetRouteName];
     args.push.apply(args, params);
-    args.push({ queryParams: queryParams.values });
+    args.push({ queryParams: queryParams });
 
     return router.generate.apply(router, args);
   }
--- tests/dummy/app/routes/qps.js
+++ tests/dummy/app/routes/qps.js
@@ -12,4 +12,4 @@ export default Em.Route.extend({
   model() {
     return pets;
   }
-})
+});
