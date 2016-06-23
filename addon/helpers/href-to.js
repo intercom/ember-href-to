1b70f179a86ee462029eae382c40f38c6b7c25ae
cibernox <miguel.camba@gmail.com>
2016-06-23 16:31:18 +0100

    Use routing service

--- addon/helpers/href-to.js
+++ addon/helpers/href-to.js
@@ -1,33 +1,24 @@
 import Em from 'ember';
-import getOwner from 'ember-getowner-polyfill';
 
 export default Em.Helper.extend({
-  // _routing: Em.inject.service('-routing'),
+  _routing: Em.inject.service('-routing'),
 
-  // onQpsChange: Em.observer('_routing.currentState', function() {
-  //   this.recompute();
-  // }),
+  onQpsChange: Em.observer('_routing.currentState', function() {
+    this.recompute();
+  }),
 
   compute(params) {
-    let router = getOwner(this).lookup('router:main');
-    if (router === undefined || router.router === undefined) {
-      return;
-    }
-
     let lastParam = params[params.length - 1];
 
     let queryParams = {};
     if (lastParam && lastParam.isQueryParams) {
       queryParams = params.pop().values;
     }
-    // let currentQueryParams = this.get('_routing.currentState.routerJsState.queryParams');
-    // queryParams = Em.merge({}, currentQueryParams, queryParams);
+    let routing = this.get('_routing');
+    let currentQueryParams = routing.get('currentState.routerJsState.queryParams');
+    queryParams = Em.merge(Em.merge({}, currentQueryParams), queryParams);
     let targetRouteName = params.shift();
 
-    let args = [targetRouteName];
-    args.push.apply(args, params);
-    args.push({ queryParams: queryParams });
-
-    return router.generate.apply(router, args);
+    return routing.generateURL(targetRouteName, params, queryParams);
   }
 });
