76375656bf74d98b6643e1b39f2986748680282c
Adam Byrne <adam.byrne@intercom.io>
2018-11-29 11:55:35 +0000

    Generate the URLs using the same approach as Ember's link-to component
    This will make it easier to use href-to in component integration tests

--- addon/helpers/href-to.js
+++ addon/helpers/href-to.js
@@ -3,8 +3,18 @@ import { getOwner } from '@ember/application';
 
 function hrefTo(context, targetRouteName, ...rest) {
   let router = getOwner(context).lookup('service:router');
+  // from https://github.com/emberjs/ember.js/blob/ad06dadce4be33db0cd74e4fd59c6dc45ceb476a/packages/%40ember/-internals/routing/lib/services/routing.ts#L45-L48
+  // similarly to Ember's internal routing service, we should return early if there's no routerMicrolib (i.e. we are rendering in an integration test)
+  if (router === undefined) {
+    return;
+  }
 
-  if(router === undefined) {
+  // from https://github.com/rwjblue/ember-router-service-polyfill/blob/82710b72d7caff1fe78b223a41ff6761d1193656/vendor/ember-router-service-polyfill/index.js#L57-L59
+  //
+  // this._router._routerMicrolib => 2.13+
+  // this._router.router => < 2.13
+  let routerMicrolib = router._router._routerMicrolib || router._router.router;
+  if (routerMicrolib === undefined) {
     return;
   }
 
