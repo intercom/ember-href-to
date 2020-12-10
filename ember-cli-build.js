1b0357a964c6787aa2981778443de543c8cf1af9
Gavin Joyce <gavinjoyce@gmail.com>
2020-12-10 11:10:03 +0000

    try Optomized Emroider Safe

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -14,6 +14,17 @@ module.exports = function(defaults) {
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
 
-  const { maybeEmbroider } = require('@embroider/test-setup');
-  return maybeEmbroider(app);
+  // const { maybeEmbroider } = require('@embroider/test-setup');
+  // return maybeEmbroider(app);
+
+  const { Webpack } = require('@embroider/webpack');
+  return require('@embroider/compat').compatBuild(app, Webpack, {
+    // staticAddonTestSupportTrees: true,
+    // staticAddonTrees: true,
+    // staticHelpers: true,
+    // staticComponents: true,
+    // packagerOptions: {
+    //    webpackConfig: { }
+    // }
+  });
 };
