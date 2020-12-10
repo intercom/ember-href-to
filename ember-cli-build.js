f8aeac3188d0cf94a732ae8495b3909cacbf8b39
Gavin Joyce <gavinjoyce@gmail.com>
2020-12-10 15:18:07 +0000

    `staticAddonTestSupportTrees` and `staticAddonTrees`

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -19,8 +19,8 @@ module.exports = function(defaults) {
 
   const { Webpack } = require('@embroider/webpack');
   return require('@embroider/compat').compatBuild(app, Webpack, {
-    // staticAddonTestSupportTrees: true,
-    // staticAddonTrees: true,
+    staticAddonTestSupportTrees: true,
+    staticAddonTrees: true,
     // staticHelpers: true,
     // staticComponents: true,
     // packagerOptions: {
