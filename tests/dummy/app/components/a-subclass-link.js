2a7733ce17569467acfed7ee6c76c51120c53d9e
Miguel Andrade <jmandrade0@gmail.com>
2024-01-10 12:21:47 +0000

    do not rely on ember-legacy-built-in-components (#152)
    * do not rely on ember-legacy-built-in-components
    
    * add link-to case

---
+++ tests/dummy/app/components/a-subclass-link.js
@@ -0,0 +1,16 @@
+import ALink from './a-link';
+
+let LinkClass = ALink.extend({
+});
+
+LinkClass = LinkClass.extend({
+});
+
+LinkClass = LinkClass.extend({
+});
+
+LinkClass = LinkClass.extend({
+});
+
+
+export default LinkClass;
